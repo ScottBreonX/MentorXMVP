19fcc8a15dc508398d77a0c5c6f2b2b5b26ee3cd
ScottBreonX <scottbreonx@gmail.com>
2021-01-17 16:33:23 -0800

    Added year in school field to profile registration; TODO --> finish layout of profile page and editing functions

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -1,21 +1,16 @@
-import 'package:flutter/foundation.dart';
-import 'package:mentorx_mvp/models/user.dart';
-
 class ProfileModel {
   ProfileModel({
-    this.fName = '',
-    this.lName = '',
-    this.email = '',
-    this.major = '',
-    this.submitted = false,
-    this.showSpinner = false,
+    this.fName,
+    this.lName,
+    this.email,
+    this.major,
+    this.yearInSchool,
   });
   final String fName;
   final String lName;
   final String major;
+  final String yearInSchool;
   final String email;
-  final bool submitted;
-  final bool showSpinner;
 
   factory ProfileModel.fromMap(Map<String, dynamic> data) {
     if (data == null) {
@@ -24,8 +19,14 @@ class ProfileModel {
     final String fName = data['First Name'];
     final String lName = data['Last Name'];
     final String major = data['Major'];
+    final String yearInSchool = data['Year in School'];
     final String email = data['Email Address'];
-    return ProfileModel(fName: fName, lName: lName, email: email, major: major);
+    return ProfileModel(
+        fName: fName,
+        lName: lName,
+        email: email,
+        major: major,
+        yearInSchool: yearInSchool);
   }
 
   Map<String, dynamic> toMap() {
@@ -34,24 +35,7 @@ class ProfileModel {
       'Last Name': lName,
       'Major': major,
       'Email Address': email,
+      'Year in School': yearInSchool,
     };
-  }
-
-  ProfileModel copyWith({
-    String email,
-    String fName,
-    String lName,
-    String major,
-    bool submitted,
-    bool showSpinner,
-  }) {
-    return ProfileModel(
-      email: email ?? this.email,
-      fName: fName ?? this.fName,
-      lName: lName ?? this.lName,
-      major: major ?? this.major,
-      submitted: submitted ?? this.submitted,
-      showSpinner: showSpinner ?? this.showSpinner,
-    );
   }
 }
