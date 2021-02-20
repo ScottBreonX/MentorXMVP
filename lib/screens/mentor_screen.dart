350c80afaa311c9e1b06e0fb90aeac71ff49c0cf
ScottBreonX <scottbreonx@gmail.com>
2021-02-19 20:52:37 -0800

    Rehasing Mentor enrollment

--- lib/screens/mentor_screen.dart
+++ lib/screens/mentor_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mentorx_mvp/components/bottom_navigation_bar.dart';
 import 'package:mentorx_mvp/components/rounded_button.dart';
 import 'package:mentorx_mvp/constants.dart';
 
@@ -51,31 +52,46 @@ class _MentorScreenState extends State<MentorScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (profileData == null) {
+      return Center(
+        child: CircularProgressIndicator(
+          backgroundColor: kMentorXTeal,
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kMentorXTeal,
-        title: Text('Mentoring'),
+        title: Text('Mentor Enrollment'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              Navigator.pushNamed(context, XBottomNavigationBar.id);
+            },
+          ),
+        ],
       ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                RoundedButton(
-                  onPressed: () {},
-                  minWidth: 500.0,
-                  title: 'Questionnaire',
-                  buttonColor: kMentorXTeal,
-                  fontColor: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Why be a mentor?'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Enter a search term',
                 ),
-              ],
+              ),
             ),
-          ),
+//                      TODO: qualitative on what makes you a good mentor? // what experience can you help?
+//                      TODO: Name, Major, Year in School on profile cards... remove email address
+//                      TODO: filter on available mentors (Year in School, Major, other binary filters
+          ],
         ),
       ),
     );
