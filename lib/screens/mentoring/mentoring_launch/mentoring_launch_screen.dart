5dba9882e1b72bad0865510828baf6b52f28418f
ScottBreonX <scottbreonx@gmail.com>
2022-04-27 09:08:37 -0700

    mentor launch screen UI complete

--- lib/screens/mentoring/mentoring_launch/mentoring_launch_screen.dart
+++ lib/screens/mentoring/mentoring_launch/mentoring_launch_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mentorx_mvp/components/icon_card.dart';
+import 'package:mentorx_mvp/components/icon_circle.dart';
 import 'package:mentorx_mvp/models/user.dart';
 import '../../../components/progress.dart';
 import '../../../models/match_model.dart';
@@ -32,6 +36,9 @@ class _MentoringLaunchScreenState extends State<MentoringLaunchScreen> {
     super.initState();
   }
 
+  bool userProfilePic = false;
+  bool mentorProfilePic = false;
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<Object>(
@@ -46,20 +53,224 @@ class _MentoringLaunchScreenState extends State<MentoringLaunchScreen> {
           }
           MatchModel matchInfo = MatchModel.fromDocument(snapshot.data);
 
-          return Scaffold(
-            appBar: AppBar(
-              elevation: 5,
-              title: Image.asset(
-                'assets/images/MentorPinkWhite.png',
-                height: 150,
-              ),
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                child: Text('${matchInfo.notes}'),
-              ),
-            ),
-          );
+          return StreamBuilder<Object>(
+              stream: usersRef.doc(widget.loggedInUser.id).snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return circularProgress();
+                }
+                myUser user = myUser.fromDocument(snapshot.data);
+                return StreamBuilder<Object>(
+                    stream: usersRef.doc(widget.mentorUID).snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return circularProgress();
+                      }
+                      myUser mentor = myUser.fromDocument(snapshot.data);
+
+                      if (user.profilePicture == null ||
+                          user.profilePicture == "") {
+                        userProfilePic = false;
+                      } else {
+                        userProfilePic = true;
+                      }
+                      if (mentor.profilePicture == null ||
+                          mentor.profilePicture == "") {
+                        mentorProfilePic = false;
+                      } else {
+                        mentorProfilePic = true;
+                      }
+
+                      return Scaffold(
+                        appBar: AppBar(
+                          elevation: 5,
+                          title: Image.asset(
+                            'assets/images/MentorPinkWhite.png',
+                            height: 150,
+                          ),
+                        ),
+                        body: SingleChildScrollView(
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 50.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          ImageCircle(
+                                              profilePicBool: userProfilePic,
+                                              myUserRef: user),
+                                          Container(
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 10.0),
+                                              child: Text(
+                                                '${user.firstName} ${user.lastName}',
+                                                style: TextStyle(
+                                                  fontFamily: 'WorkSans',
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black45,
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          ImageCircle(
+                                              profilePicBool: mentorProfilePic,
+                                              myUserRef: mentor),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                top: 10.0),
+                                            child: Container(
+                                              child: Text(
+                                                '${mentor.firstName} ${mentor.lastName}',
+                                                style: TextStyle(
+                                                  fontFamily: 'WorkSans',
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black45,
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0, right: 20),
+                                  child: Divider(
+                                    thickness: 2,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0, right: 20.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      IconCard(
+                                        boxHeight: 140,
+                                        boxWidth: 140,
+                                        iconSize: 60,
+                                        cardColor: Colors.white,
+                                        cardIcon:
+                                            CupertinoIcons.chat_bubble_2_fill,
+                                        cardText: 'Chat',
+                                      ),
+                                      IconCard(
+                                        boxHeight: 140,
+                                        boxWidth: 140,
+                                        iconSize: 60,
+                                        cardColor: Colors.white,
+                                        cardIcon: Icons.map,
+                                        cardText: 'Guides',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0, right: 20.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      IconCard(
+                                        boxHeight: 140,
+                                        boxWidth: 140,
+                                        iconSize: 60,
+                                        cardColor: Colors.white,
+                                        cardIcon: Icons.note_rounded,
+                                        cardText: 'Notes',
+                                      ),
+                                      IconCard(
+                                        boxHeight: 140,
+                                        boxWidth: 140,
+                                        iconSize: 60,
+                                        cardColor: Colors.white,
+                                        cardIcon: Icons.settings,
+                                        cardText: 'Manage',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+              });
         });
+  }
+}
+
+class ImageCircle extends StatelessWidget {
+  const ImageCircle({
+    Key key,
+    @required this.profilePicBool,
+    @required this.myUserRef,
+  }) : super(key: key);
+
+  final bool profilePicBool;
+  final myUser myUserRef;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: profilePicBool ? 75 : 70,
+      backgroundColor: Colors.white,
+      child: CircleAvatar(
+        radius: 72,
+        backgroundColor: Colors.white,
+        child: !profilePicBool
+            ? CircleAvatar(
+                radius: 72,
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Colors.white,
+                ),
+              )
+            : CachedNetworkImage(
+                imageUrl: myUserRef.profilePicture,
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Colors.white,
+                ),
+              ),
+      ),
+    );
   }
 }
