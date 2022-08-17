0c7006994465ac9ed02412cd87eb71d34afa2cc5
ScottBreonX <scottbreonx@gmail.com>
2022-08-16 18:57:10 -0700

    fixed navigation issue after joining program

--- lib/screens/programs/program_join_request.dart
+++ lib/screens/programs/program_join_request.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mentorx_mvp/components/rounded_button.dart';
 import 'package:mentorx_mvp/models/program.dart';
+import 'package:mentorx_mvp/screens/home_screen/home_screen.dart';
 import 'package:mentorx_mvp/screens/launch_screen.dart';
 import 'package:mentorx_mvp/screens/programs/program_launch/program_enrollment_screen.dart';
 
@@ -222,8 +223,8 @@ class _ProgramJoinRequestState extends State<ProgramJoinRequest> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => LaunchScreen(
-                                  pageIndex: 1,
+                                builder: (context) => HomeScreen(
+                                  loggedInUser: loggedInUser,
                                 ),
                               ));
                         },
